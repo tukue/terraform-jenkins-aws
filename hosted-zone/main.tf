920586a5f25eaae4ce550a4628d93a5a86e7aa29
Tukue Gebregergis <tukue.geb@gmail.com>
2025-05-22 12:10:50 +0200

    updating domain name

--- hosted-zone/main.tf
+++ hosted-zone/main.tf
@@ -8,13 +8,13 @@ locals {
   }
 }
 
-data "aws_route53_zone" "dev_proj_1_jhooq_org" {
-  name         = "jhooq.org"
+data "aws_route53_zone" "dev_proj_1_new_domain" {
+  name         = "newdomain.com"  # Replace with your new domain name
   private_zone = false
 }
 
 resource "aws_route53_record" "lb_record" {
-  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  zone_id = data.aws_route53_zone.dev_proj_1_new_domain.zone_id
   name    = var.domain_name
   type    = "A"
 
