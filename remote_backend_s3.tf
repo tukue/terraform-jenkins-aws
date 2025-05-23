d0081b520370f5509bd3800f49879f454b11248e
Tukue Gebregergis <tukue.geb@gmail.com>
2025-05-23 10:37:36 +0200

    adding dynamo db resource for state terraform storage

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-tfstate-dev-2025"
-    key    = "terraform/dev/terraform.tfstate"
-    region = "eu-north-1"
-    encrypt = true
+    bucket         = "jenkins-tfstate-dev-2025"
+    key            = "terraform/dev/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "jenkins-terraform-locks"
   }
 }
--- s3.tf
+++ s3.tf
@@ -36,3 +36,19 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   restrict_public_buckets = true
 }
 
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "jenkins-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock Table"
+  }
+}
+
